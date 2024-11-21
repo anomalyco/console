7f611c3074ec0bdb80011e4590d0c174d40c595c
Dax Raad <d@ironbay.co>
2024-11-21 15:07:12 -0500

    format

--- infra/planetscale.ts
+++ infra/planetscale.ts
@@ -3,13 +3,17 @@ const mysql = planetscale.Database.get("Database", "sst,sst");
 const branch =
   $app.stage === "production"
     ? planetscale.Branch.get("DatabaseBranch", "sst,sst,production")
-    : new planetscale.Branch("DatabaseBranch", {
-        database: mysql.name,
-        organization: mysql.organization,
-        name: $app.stage,
-        parentBranch: "production",
-        production: $app.stage === "production",
-      });
+    : new planetscale.Branch(
+        "DatabaseBranch",
+        {
+          database: mysql.name,
+          organization: mysql.organization,
+          name: $app.stage,
+          parentBranch: "production",
+          production: $app.stage === "production",
+        },
+        {},
+      );
 
 const password = new planetscale.Password("DatabasePassword", {
   database: mysql.name,
@@ -21,10 +25,10 @@ const password = new planetscale.Password("DatabasePassword", {
 
 export const database = new sst.Linkable("Database", {
   properties: {
-    username: password.username,
     host: branch.mysqlAddress,
-    password: password.plaintext,
+    username: password.username,
     database: password.database,
+    password: password.plaintext,
     port: 3306,
   },
 });
