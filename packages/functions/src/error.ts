9c824d72bfe2e83475eec6b705d3b8823477dc66
Dax Raad <d@ironbay.co>
2025-01-20 14:02:13 -0500

    test

--- packages/functions/src/error.ts
+++ packages/functions/src/error.ts
@@ -1,6 +1,6 @@
 export const handler = async () => {
   console.log("starting", new Date());
-  console.error(new Error("logged error 3"));
+  console.error(new Error("logged a different error"));
   return {
     statusCode: 200,
     body: "ok",
