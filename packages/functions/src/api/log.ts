83e5c7edfbb5fb305f9ec5723a62041fc495e1a0
Dax Raad <d@ironbay.co>
2025-02-04 16:22:13 -0500

    remove unnecesary sort

--- packages/functions/src/api/log.ts
+++ packages/functions/src/api/log.ts
@@ -274,10 +274,6 @@ export const LogRoute = new Hono()
               }
 
               if (query.hint === "normal") {
-                results.sort((b, a) =>
-                  a[0]!.value!.localeCompare(b[0]!.value!),
-                );
-                // process in descending order, can stop after 50
                 for (const result of results) {
                   const timestamp = new Date(result[0]?.value! + " Z");
                   const length = entries.push({
