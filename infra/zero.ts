efa389b07378010a945adadfd2806b7d8eb1e0ae
Dax Raad <d@ironbay.co>
2025-02-18 13:16:08 -0500

    bump

--- infra/zero.ts
+++ infra/zero.ts
@@ -29,7 +29,7 @@ const zeroEnv = {
   ...($dev
     ? {}
     : {
-        ZERO_LITESTREAM_BACKUP_URL: $interpolate`s3://${storage.name}/zero/6`,
+        ZERO_LITESTREAM_BACKUP_URL: $interpolate`s3://${storage.name}/zero/7`,
       }),
 };
 
