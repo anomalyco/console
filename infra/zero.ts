edc0260b0e1710829755e1489bb955d2c4a63f27
Dax Raad <d@ironbay.co>
2025-02-19 14:04:15 -0500

    false

--- infra/zero.ts
+++ infra/zero.ts
@@ -85,7 +85,7 @@ const replication = !$dev
     })
   : undefined;
 
-if ($app.stage === "production") {
+if ($app.stage === "production" && false) {
   new sst.aws.Service(`ZeroReplicationTest`, {
     cluster,
     ...($app.stage === "production"
